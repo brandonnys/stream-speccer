bb5980e829cbb23bc90de204cc5dec7a5bc8740a
Brandon Nys <brandonnys@gmail.com>
2023-03-18 20:56:15 -0700

    Converting to the client-side

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
 const express = require('express');
-const si = require('systeminformation');
-const speedTest = require('speedtest-net');
 const { Pool } = require('pg');
-const sqlite3 = require('sqlite3').verbose();
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json()); // Add this line to parse incoming JSON data
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.get('/submit', async (req, res) => {
-  const [systemData, speedData] = await Promise.all([collectSystemInformation(), performSpeedtest()]);
-  saveToDatabase(systemData, speedData);
+app.post('/submit', async (req, res) => { // Change this line to handle a POST request
+  const data = req.body;
+  const systemData = data.systemData;
+  const speedData = data.speedData;
+  await saveToDatabase(systemData, speedData);
   res.json({ systemData, speedData });
 });
-
-const collectSystemInformation = async () => {
-  const cpuData = await si.cpu();
-  const baseboardData = await si.baseboard();
-  const gpuData = await si.graphics();
-  const ramData = await si.mem();
-
-  const cpuModel = cpuData.brand;
-  const motherboardVendor = baseboardData.manufacturer;
-  const motherboardModel = baseboardData.model;
-  const gpuVendor = gpuData.controllers.length > 0 ? gpuData.controllers[0].vendor : 'Unknown';
-  const gpuModel = gpuData.controllers.length > 0 ? gpuData.controllers[0].model : 'Unknown';
-  const ram = (ramData.total / (1024 ** 3)).toFixed(2);
-
-  return { cpuModel, motherboardVendor, motherboardModel, gpuVendor, gpuModel, ram };
-};
-
-const performSpeedtest = async () => {
-  const options = { acceptLicense: true, acceptGdpr: true };
-  const result = await speedTest(options);
-  const downloadSpeed = (result.download.bandwidth / 125000).toFixed(2);
-  const uploadSpeed = (result.upload.bandwidth / 125000).toFixed(2);
-
-  return { downloadSpeed, uploadSpeed };
-};
 
 const saveToDatabase = async (systemData, speedData) => {
   const connectionString = process.env.DATABASE_URL;
@@ -50,8 +25,8 @@ const saveToDatabase = async (systemData, speedData) => {
     ssl: { rejectUnauthorized: false },
   });
 
-  const query = `INSERT INTO your_table_name (cpu, motherboard_vendor, motherboard_model, gpu_vendor, gpu_model, ram, download_speed, upload_speed) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
-  const values = [systemData.cpuModel, systemData.motherboardVendor, systemData.motherboardModel, systemData.gpuVendor, systemData.gpuModel, systemData.ram, speedData.downloadSpeed, speedData.uploadSpeed];
+  const query = `INSERT INTO speccerdb (user_agent, platform, language, hardware_concurrency, download_speed) VALUES ($1, $2, $3, $4, $5)`;
+  const values = [systemData.userAgent, systemData.platform, systemData.language, systemData.hardwareConcurrency, speedData.speed];
 
   await pool.query(query, values);
   pool.end();
@@ -59,9 +34,3 @@ const saveToDatabase = async (systemData, speedData) => {
 
 const PORT = process.env.PORT || 5432;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-module.exports = {
-  collectSystemInformation,
-  performSpeedtest,
-  saveToDatabase,
-};
